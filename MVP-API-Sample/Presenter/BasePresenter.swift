1c799bec01f11e8fb9c34e568a9ea963dcb8be38
ddd503 <skbsporta7@gmail.com>
2018-09-30 00:19:58 +0900

    Presenterクラスの破棄処理を追加

--- MVP-API-Sample/Presenter/BasePresenter.swift
+++ MVP-API-Sample/Presenter/BasePresenter.swift
@@ -7,20 +7,28 @@
 //
 
 protocol BasePresenter: class {
-     // それぞれの準拠元で柔軟的に型を決定できる値
+    // それぞれの準拠元で柔軟的に型を決定できる値
     associatedtype ViewObject
     // Presenterクラス側でViewクラスを保持する用propatie
     // デフォルト実装で、自動的に渡されたViewクラスをPresenterクラスのinterface変数(protcol)にセットする
     var interface: ViewObject? { get set }
     
     func applyInterface(view: ViewObject)
+    
+    func destroyInterface()
 }
 
+/// 今回はデフォルト実装でPresenter側でのViewクラスの保持方を共通化
 extension BasePresenter {
-    /// Presenterクラス側で状態を管理するViewクラスを設定する(今回はデフォルト実装で共通化)
+    /// Presenterクラス側で状態を管理するViewクラスを設定する()
     ///
     /// - Parameter view: viewクラス
     func applyInterface(view: ViewObject) {
         interface = view
+    }
+    
+    /// presenterはdeinit時にこれを呼んで自身が持つViewクラスインスタンスを破棄する（相互参照でVとMが消えないことの防止）
+    func destroyInterface() {
+        interface = nil
     }
 }
